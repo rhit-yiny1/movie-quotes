e10e3e3e4ac9acc90152bb920055fdc61280f71a
rhit-yiny1 <89856239+rhit-yiny1@users.noreply.github.com>
2023-01-16 15:26:55 -0500

    added firestore

--- movie_quotes/lib/main.dart
+++ movie_quotes/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:movie_quotes/pages/movie_quotes_list_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
